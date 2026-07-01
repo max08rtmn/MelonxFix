3a63c917ed45b16a52ac7d1a86eafc43983c0e87
max08rtmn <maxrotti0@gmail.com>
2026-07-01 16:43:24 +0200

    Add files via upload

--- src/Ryujinx.Horizon/Sdk/DebugUtil.cs
+++ src/Ryujinx.Horizon/Sdk/DebugUtil.cs
@@ -1,1 +1,12 @@
+using System.Diagnostics;
 
+namespace Ryujinx.Horizon.Sdk
+{
+    static class DebugUtil
+    {
+        public static void Assert(bool condition)
+        {
+            Debug.Assert(condition);
+        }
+    }
+}
